7c6e8b27fbff6bc8cf0e1aa39fae6e62f2d464ce
Felipe Nicoletti Reis Mario <felipenicolettirmario@gmail.com>
2020-04-19 12:49:05 -0300

    Serviço para validfação por SMS criado

--- routes/router.js
+++ routes/router.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 const tokenAuthentication = require('../services/tokenAuthentication');
 const connSql = require('../services/mySql')
+const smsValidator = require('../services/smsValidation');
 
 router.post('/user/put',UserController.putUser);
 
 //Rotas para autenticação e logout do sistema
 router.post("/auth",connSql,tokenAuthentication.authenticateUser)
 router.get("/logout",tokenAuthentication.verifyJWT,tokenAuthentication.clearToken)
+
+//Rotas SMS
+router.post("/sms",connSql,smsValidator.sendSMS)
+router.post("/validadeSMS",connSql,smsValidator.validateNumber)
 
 module.exports = router;
