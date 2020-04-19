7f3f82ab2da1399557ac700dbde399e806a3be2b
toledompm <toledompm@gmail.com>
2020-04-19 15:26:31 -0300

    Added cors to routes, updated User post

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = require('./routes/router');
+const cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || '3000';
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
